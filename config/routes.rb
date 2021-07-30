876104a6c14b7c452e796a16f395aba6b7a69ee9
青木大地 <e5khtamcx@i.softbank.jp>
2021-07-30 05:40:49 +0000

    Rails5課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'hoge', to: 'users#new'
+  
+  #get 'users/new', to:'users#new'
+  #get 'users/new'
+  resources :users
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
