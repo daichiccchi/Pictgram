b6492f529d25ed295a33fd1ec9276edb4fb44762
青木大地 <e5khtamcx@i.softbank.jp>
2021-07-30 11:12:10 +0000

    Rails06課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'hoge', to: 'users#new'
   
   #get 'users/new', to:'users#new'
@@ -7,4 +8,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
 end
